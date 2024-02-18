c029b680e98a36aee7d7d076a2cbdb27868d9fd5
Thiago Barros <thiagobarros95@gmail.com>
2024-02-17 21:30:16 -0300

    feat: create random key

---
+++ app/Bussiness/Domain/ValueObjects/RandomKey.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Domain\ValueObjects;
+
+final readonly class RandomKey
+{
+    private string $value;
+
+    public function __construct()
+    {
+        $this->value = $this->generate();
+    }
+
+    private function generate(): string
+    {
+        $firstPart = random_bytes(8);
+        $secondPart = random_bytes(4);
+        $thirdPart = random_bytes(4);
+        $fourthPart = random_bytes(4);
+        $fifthPart = random_bytes(12);
+
+        return "$firstPart-$secondPart-$thirdPart-$fourthPart-$fifthPart";
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
