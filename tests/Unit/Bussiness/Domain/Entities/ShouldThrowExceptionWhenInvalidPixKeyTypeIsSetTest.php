01662b2877bb76634cb7beb4d97b2d81cffb176c
Thiago Barros <thiagobarros95@gmail.com>
2024-02-17 21:27:07 -0300

    test: validate that internal user can not have pix key of type cnpj

---
+++ tests/Unit/Bussiness/Domain/Entities/ShouldThrowExceptionWhenInvalidPixKeyTypeIsSetTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Bussiness\Domain\Entities\PixKey;
+use App\Bussiness\Domain\Enums\PixKeyType;
+
+it('Should throw exception when invalid pixkey type is set', function () {
+    PixKey::reset()
+        ->setType(PixKeyType::CNPJ);
+})->throws(
+    DomainException::class,
+    'Invalid pix key type. Internal user can not have a pix key of type CNPJ.',
+    400
+);
