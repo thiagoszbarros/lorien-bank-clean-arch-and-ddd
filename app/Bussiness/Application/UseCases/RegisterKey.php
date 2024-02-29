ad5a0dd5c13f12b06c3b7c3beda8235d592237af
Thiago Barros <thiagobarros95@gmail.com>
2024-02-28 22:08:47 -0300

    feat: add register key use case

---
+++ app/Bussiness/Application/UseCases/RegisterKey.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Application\UseCases;
+
+use App\Bussiness\Application\Dtos\RegisterKeyInput;
+use App\Bussiness\Application\Dtos\RegisterKeyOutput;
+use App\Bussiness\Domain\Entities\PixKey;
+use App\Bussiness\Domain\Enums\Messages;
+use App\Bussiness\Domain\Enums\PixKeyType;
+use App\Bussiness\Domain\Repositories\IRegisterKey;
+use App\Bussiness\Domain\ValueObjects\AccountNumber;
+use App\Bussiness\Domain\ValueObjects\Cellphone;
+use App\Bussiness\Domain\ValueObjects\Cpf;
+use App\Bussiness\Domain\ValueObjects\Email;
+use App\Bussiness\Domain\ValueObjects\RandomKey;
+
+final readonly class RegisterKey
+{
+    public function __construct(private IRegisterKey $registerKeyRepo)
+    {
+    }
+
+    public function handle(RegisterKeyInput $input): RegisterKeyOutput
+    {
+        $pixKeyType = PixKeyType::tryFrom($input->getKeyType());
+
+        if ($pixKeyType === null || $pixKeyType === PixKeyType::CNPJ) {
+            return new RegisterKeyOutput(false, Messages::INVALID_PIX_KEY_TYPE_GIVEN);
+        }
+
+        $key = match ($pixKeyType) {
+            PixKeyType::CPF => new Cpf($input->getKey()),
+            PixKeyType::EMAIL => new Email($input->getKey()),
+            PixKeyType::CELLPHONE => new Cellphone($input->getKey()),
+            PixKeyType::RANDOM => new RandomKey(),
+        };
+
+        $pixKey = PixKey::reset()
+            ->setKey($key)
+            ->setType($pixKeyType)
+            ->setAccountNumber(
+                new AccountNumber(
+                    new Cpf($input->getAccountNumber())
+                )
+            );
+
+        return $this->registerKeyRepo->register($pixKey) === true
+            ? new RegisterKeyOutput(true, Messages::PIX_KEY_SUCCESSESFULLY_REGISTERED)
+            : new RegisterKeyOutput(false, Messages::FAILED_TO_REGISTER_KEY);
+    }
+}
