91f0a2f3f5e8d886c9b2f6ba4c6b515c425ffacc
Thiago Barros <thiagobarros95@gmail.com>
2024-02-28 22:07:57 -0300

    feat: add register key input

---
+++ app/Bussiness/Application/Dtos/RegisterKeyInput.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Application\Dtos;
+
+final readonly class RegisterKeyInput
+{
+    public function __construct(
+        private int $keyType,
+        private ?string $key,
+        private string $accountNumber,
+    ) {
+    }
+
+    public function getKeyType(): int
+    {
+        return $this->keyType;
+    }
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    public function getAccountNumber(): string
+    {
+        return $this->accountNumber;
+    }
+}
