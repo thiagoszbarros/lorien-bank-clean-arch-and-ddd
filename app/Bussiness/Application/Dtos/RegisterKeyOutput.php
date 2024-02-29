a0e2de3a150974cb2fad95892bfbfa4778043782
Thiago Barros <thiagobarros95@gmail.com>
2024-02-28 22:08:09 -0300

    feat: add register key output

---
+++ app/Bussiness/Application/Dtos/RegisterKeyOutput.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Application\Dtos;
+
+use App\Bussiness\Domain\Enums\Messages;
+
+class RegisterKeyOutput
+{
+    public function __construct(
+        private bool $success,
+        private Messages $message,
+    ) {
+    }
+
+    public function getSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message->value;
+    }
+}
