267cf274b078818dada21a19d4b2afe46bdab5fa
Thiago Barros <thiagobarros95@gmail.com>
2024-02-28 22:20:07 -0300

    feat: add register key repository interface

---
+++ app/Bussiness/Domain/Repositories/IRegisterKey.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Domain\Repositories;
+
+use App\Bussiness\Domain\Entities\PixKey;
+
+interface IRegisterKey
+{
+    public function register(PixKey $pixKey): bool;
+}
