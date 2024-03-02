9a7832573106876be43554bd6f2289c698b60053
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 01:52:28 -0300

    chore: solving dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(
+            \App\Bussiness\Domain\Repositories\IRegisterKey::class,
+            \App\Repositories\Eloquent\RegisterKey::class
+        );
+        $this->app->bind(
+            \App\Bussiness\Domain\Repositories\IGetPixKeyByCheckingAccountIdAndType::class,
+            \App\Repositories\Eloquent\GetPixKeyByCheckingAccountIdAndType::class
+        );
+        $this->app->bind(
+            \App\Bussiness\Domain\Repositories\IGetCheckingAccountByNumber::class,
+            \App\Repositories\Eloquent\GetCheckingAccountByNumber::class
+        );
     }
 }
