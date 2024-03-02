5b72921adfe106610e66f50c765e118c10b39b69
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 01:33:47 -0300

    style: apply lint

--- database/factories/CheckingAccountFactory.php
+++ database/factories/CheckingAccountFactory.php
@@ -4,8 +4,8 @@ namespace Database\Factories;
 
 use App\Bussiness\Domain\ValueObjects\LorienBankNumber;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as FakerFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CheckingAccount>
@@ -20,6 +20,7 @@ class CheckingAccountFactory extends Factory
     public function definition(): array
     {
         $faker = FakerFactory::create('pt_BR');
+
         return [
             'user_id' => User::whereEmail('test@example.com')->first()->id,
             'bank' => (new LorienBankNumber())->getValue(),
