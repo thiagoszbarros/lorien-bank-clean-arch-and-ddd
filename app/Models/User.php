6e4a3082d34a7554d438a5a53c02a9d241ddf441
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 00:09:08 -0300

    feat: add checking acount relation with checking user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function checkingAccount(): HasOne
+    {
+        return $this->hasOne(CheckingAccount::class);
+    }
 }
