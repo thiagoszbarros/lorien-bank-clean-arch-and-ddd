77a58d4dd32e1df7c7a30a097ba37ccfca60951e
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 01:30:34 -0300

    feat: add get checking account by number interface

---
+++ app/Bussiness/Domain/Repositories/IGetCheckingAccountByNumber.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Domain\Repositories;
+
+use App\Bussiness\Domain\Entities\CheckingAccount;
+use App\Bussiness\Domain\ValueObjects\AccountNumber;
+
+interface IGetCheckingAccountByNumber
+{
+    public function get(AccountNumber $accountNumber): ?CheckingAccount;
+}
