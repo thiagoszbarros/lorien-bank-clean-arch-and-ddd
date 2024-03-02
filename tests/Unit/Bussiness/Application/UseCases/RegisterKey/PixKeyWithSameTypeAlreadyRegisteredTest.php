bd8de7d2f010490da6814997f1e62fb651ada1f9
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 01:51:37 -0300

    test: add test over pix key type already registered scenario

---
+++ tests/Unit/Bussiness/Application/UseCases/RegisterKey/PixKeyWithSameTypeAlreadyRegisteredTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Bussiness\Application\Dtos\RegisterKeyInput;
+use App\Bussiness\Application\UseCases\RegisterKey;
+use App\Bussiness\Domain\Entities\CheckingAccount;
+use App\Bussiness\Domain\Entities\PixKey;
+use App\Bussiness\Domain\Enums\PixKeyType;
+use App\Bussiness\Domain\Repositories\IGetCheckingAccountByNumber;
+use App\Bussiness\Domain\Repositories\IGetPixKeyByCheckingAccountIdAndType;
+use App\Bussiness\Domain\Repositories\IRegisterKey;
+use Mockery;
+
+it('should return failed to register key error message', function (): void {
+    $this->getCheckingAccountByNumberRepo = Mockery::mock(IGetCheckingAccountByNumber::class);
+
+    $this->getPixKeyByCheckingAccountIdAndTypeRepo = Mockery::mock(IGetPixKeyByCheckingAccountIdAndType::class);
+
+    $this->registerKeyRepo = Mockery::mock(IRegisterKey::class);
+
+    $this->useCase = new RegisterKey(
+        getCheckingAccountByNumberRepo: $this->getCheckingAccountByNumberRepo,
+        getPixKeyByCheckingAccountIdAndTypeRepo: $this->getPixKeyByCheckingAccountIdAndTypeRepo,
+        registerKeyRepo: $this->registerKeyRepo,
+    );
+
+    $inputBoundary = new RegisterKeyInput(
+        PixKeyType::CPF->value,
+        '06806573398',
+        '06806573398'
+    );
+
+    $this->getCheckingAccountByNumberRepo
+        ->shouldReceive('get')
+        ->andReturn(CheckingAccount::reset());
+
+    $this->getPixKeyByCheckingAccountIdAndTypeRepo
+        ->shouldReceive('get')
+        ->andReturn(PixKey::reset());
+
+    $result = $this->useCase->handle($inputBoundary);
+
+    expect($result->getMessage())->toBe('Pix Key with same type already registered for that account number given.');
+});
