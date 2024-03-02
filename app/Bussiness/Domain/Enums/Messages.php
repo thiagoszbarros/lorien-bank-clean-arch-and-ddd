750961442314195a82cd55201fa0d6e2abbc13c7
Thiago Barros <thiagobarros95@gmail.com>
2024-03-02 00:08:11 -0300

    feat: add message key type already registered

--- app/Bussiness/Domain/Enums/Messages.php
+++ app/Bussiness/Domain/Enums/Messages.php
@@ -9,4 +9,5 @@ enum Messages: string
     case INVALID_PIX_KEY_TYPE_GIVEN = 'Invalid pix key type given. Accepted types: 1 - CPF, 2 - EMAIL, 3 - CELLPHONE, 4 - RANDOM';
     case PIX_KEY_SUCCESSESFULLY_REGISTERED = 'Pix Key successfully registered.';
     case FAILED_TO_REGISTER_KEY = 'Failed to register key.';
+    case KEY_TYPE_ALREADY_REGISTERED = 'Pix Key with same type already registered for that account number given.';
 }
