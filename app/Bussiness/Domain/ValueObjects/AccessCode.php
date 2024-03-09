0f5e50549ac133183b5b9b36077598a8dd7e5bf9
Thiago Barros <thiagobarros95@gmail.com>
2024-03-09 19:10:15 -0300

    style: apply lint

--- app/Bussiness/Domain/ValueObjects/AccessCode.php
+++ app/Bussiness/Domain/ValueObjects/AccessCode.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Bussiness\Domain\ValueObjects;
 
-final readonly class AccessCode {
+final readonly class AccessCode
+{
     private string $accessCode;
 
-    public function __construct(string $accessCode) {
+    public function __construct(string $accessCode)
+    {
         if (strlen($accessCode) !== 6) {
             throw new \DomainException('PIN code access should have 6 digits.', 400);
         }
@@ -15,7 +17,8 @@ final readonly class AccessCode {
         $this->accessCode = $accessCode;
     }
 
-    public function getValue(): string {
+    public function getValue(): string
+    {
         return $this->accessCode;
     }
 }
