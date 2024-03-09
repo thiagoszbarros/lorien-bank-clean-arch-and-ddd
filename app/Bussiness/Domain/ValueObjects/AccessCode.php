5b374b66d3575ba2170c8a6729901f1ce28331e6
Thiago Barros <thiagobarros95@gmail.com>
2024-03-09 14:15:36 -0300

    feat: add access code value object

---
+++ app/Bussiness/Domain/ValueObjects/AccessCode.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bussiness\Domain\ValueObjects;
+
+final readonly class AccessCode {
+    private $accessCode;
+
+    public function __construct(string $accessCode) {
+        if (strlen($accessCode) !== 6) {
+            throw new \DomainException('PIN code access should have 6 digits.', 400);
+        }
+
+        $this->accessCode = $accessCode;
+    }
+
+    public function getValue(): string {
+        return $this->accessCode;
+    }
+}
