17b5af1f346e156a7aeb2cceae88f9ba5fdbc490
Thiago Barros <thiagobarros95@gmail.com>
2024-03-10 18:41:33 -0300

    style: apply lint

--- tests/Unit/Bussiness/Application/UseCases/RegisterKey/FailedTrytoRegisterKeyTest.php
+++ tests/Unit/Bussiness/Application/UseCases/RegisterKey/FailedTrytoRegisterKeyTest.php
@@ -4,7 +4,6 @@ use App\Bussiness\Application\Services\CreatePixKeyByType;
 use App\Bussiness\Application\UseCases\Dtos\RegisterKeyInput;
 use App\Bussiness\Application\UseCases\RegisterKey;
 use App\Bussiness\Domain\Entities\CheckingAccount\CheckingAccount;
-use App\Bussiness\Domain\Enums\Messages;
 use App\Bussiness\Domain\Enums\PixKeyType;
 use App\Bussiness\Domain\Repositories\IGetCheckingAccountByNumber;
 use App\Bussiness\Domain\Repositories\IGetPixKeyByCheckingAccountIdAndType;
