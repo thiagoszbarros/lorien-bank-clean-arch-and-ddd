3e16f5c97bc4b5386bc3eff2a4dfd97dbdbb5fdd
Thiago Barros <thiagobarros95@gmail.com>
2024-03-09 21:11:16 -0300

    refactor: refactor result presenter using builder

--- app/Bussiness/Infra/Presenters/Result.php
+++ app/Bussiness/Infra/Presenters/Result.php
@@ -6,18 +6,71 @@ namespace App\Bussiness\Infra\Presenters;
 
 final readonly class Result
 {
-    public function __construct(
-        private bool $success,
-        private ?string $message = null,
-        private mixed $data = null,
+    private bool $success;
+
+    private array $messages;
+
+    private mixed $data;
+
+    private function __construct(
     ) {
+    }
+
+    public static function reset(): static
+    {
+        return new Result();
+    }
+
+    public function getSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function setSuccess(bool $success): static
+    {
+        $this->success = $success;
+
+        return $this;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getMessages(): array
+    {
+        return $this->messages;
+    }
+
+    public function setMessages(array|string $messages): static
+    {
+        if (is_string($messages)) {
+            $this->messages = [$messages];
+
+            return $this;
+        }
+
+        $this->messages = [...$messages];
+
+        return $this;
+    }
+
+    public function getData(): mixed
+    {
+        return $this->data;
+    }
+
+    public function setData(mixed $data): static
+    {
+        $this->data = $data;
+
+        return $this;
     }
 
     public function toArray(): array
     {
         return [
             'success' => $this->success,
-            'message' => $this->message,
+            'messages' => $this->messages,
             'data' => $this->data,
         ];
     }
