d1e891fe064a2f6fa16a12d5244f8ff38480d396
Thiago Barros <thiagobarros95@gmail.com>
2024-03-10 18:31:30 -0300

    revmove: delete unnecessary case

--- app/Bussiness/Domain/Enums/Messages.php
+++ app/Bussiness/Domain/Enums/Messages.php
@@ -8,7 +8,6 @@ enum Messages: string
 {
     case INVALID_PIX_KEY_TYPE_GIVEN = 'Invalid pix key type given. Accepted types: 1 - CPF, 2 - EMAIL, 3 - CELLPHONE, 4 - RANDOM';
     case PIX_KEY_SUCCESSESFULLY_REGISTERED = 'Pix Key successfully registered.';
-    case FAILED_TO_REGISTER_KEY = 'Failed to register key.';
     case KEY_TYPE_ALREADY_REGISTERED = 'Pix Key with same type already registered for that account number given.';
     case CHECKING_ACCOUNT_NOT_FOUND = 'Checking account not found.';
     case SOMETHING_WENT_WRONG = 'Something went wrong. Try again later.';
