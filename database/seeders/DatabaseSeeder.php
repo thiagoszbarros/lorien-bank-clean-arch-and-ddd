21691be3580024e8d001cd52e23290bcdaec8b72
Thiago Barros <thiagobarros95@gmail.com>
2024-05-26 19:34:35 -0300

    feat: add seeders into database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\MaritalStatusSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        (new MaritalStatusSeeder())->run();
+        
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        (new PersonalInformationSeeder())->run();
 
         \App\Models\CheckingAccount::factory(1)->create();
     }
