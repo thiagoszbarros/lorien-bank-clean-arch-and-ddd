08048f8d5e1f2f4500eb1ba082b4b49a9e5d9595
Thiago Barros <thiagobarros95@gmail.com>
2024-05-26 20:36:34 -0300

    feat: add relation with personal information

--- app/Models/MaritalStatus.php
+++ app/Models/MaritalStatus.php
@@ -6,8 +6,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MaritalStatus extends Model
 {
     use HasFactory;
+
+    public $guarded = ['*'];
+
+    public function personalInformation(): HasMany
+    {
+        return $this->hasMany(PersonalInformation::class);
+    }
 }
